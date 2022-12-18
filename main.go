c75842c13d512026a57d9a35683350bac8dbfb55
caoyingjunz <cao.yingjunz@gmail.com>
2022-12-18 17:56:46 +0800

    add

--- main.go
+++ main.go
@@ -65,14 +65,25 @@ func main() {
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	options := ctrl.Options{
 		Scheme:                 scheme,
 		MetricsBindAddress:     metricsAddr,
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "98aadc68.pixiu.io",
-	})
+	}
+	if os.Getenv("DEBUG") == "True" {
+		// Run on localhost
+		wd, err := os.Getwd()
+		if err != nil {
+			setupLog.Error(err, "unable to get work dir")
+			os.Exit(1)
+		}
+		options.CertDir = wd + "/config/certs"
+	}
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), options)
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
