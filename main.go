2ad827a6511c20b5adfcfdaaea211091a2fffc9e
caoyingjunz <cao.yingjunz@gmail.com>
2022-12-24 16:20:21 +0800

    [part2]: Add webhook supported

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
 	var probeAddr string
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+	flag.BoolVar(&enableLeaderElection, "leader-elect", true,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
 	opts := zap.Options{
@@ -71,7 +71,7 @@ func main() {
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "98aadc68.pixiu.io",
+		LeaderElectionID:       "leaderlock.pixiu.io",
 	}
 	if os.Getenv("DEBUG") == "True" {
 		// Run on localhost
